9e20999c1d6ef2ffac83ac838d9a1b23a6990ce6
Nasima Zair <nasima.zair@gmail.com>
2026-03-02 22:12:45 -0600

    Add SwiftUI Xcode project

---
+++ PlaqueTracker/PlaqueTracker/PlaqueTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaqueTrackerApp.swift
+//  PlaqueTracker
+//
+//  Created by Gayrat Hamraev on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PlaqueTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
